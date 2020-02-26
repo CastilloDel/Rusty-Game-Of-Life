0816138b434bb1e577a825625b3f9777d4cd9333
CastilloDel <delcastillodelarosadaniel@gmail.com>
2020-02-26 09:28:05 +0000

    Implement a first draft of the cell

--- src/game_of_life/cell.rs
+++ src/game_of_life/cell.rs
@@ -1,16 +1,49 @@
-#[derive(Clone)]
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub enum States {
   Alive,
   Dead,
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct Cell {
   state :States,
+  next_state :States,
 }
 
 impl Cell {
   pub fn new() -> Cell {
-    Cell {state: States::Dead}
+    Cell {
+      state: States::Dead,
+      next_state: States::Dead}
+  }
+  pub fn update_state(&mut self) {
+    self.state = self.next_state;
+  }
+}
+
+impl fmt::Display for Cell {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    if self.state == States::Dead {
+      write!(f, " ")
+    } else {
+      write!(f, "X")
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::{States, Cell};
+  #[test]
+  fn create_cell() {
+    let mut a = Cell::new();
+    let b = Cell{state: States::Dead,
+      next_state: States::Alive,
+    };
+    a.next_state = States::Alive;
+    a.update_state();
+    assert_eq!(a, b);
   }
 }
