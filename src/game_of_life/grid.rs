cbdbc07e2c71ac1c85ebba395037a3ab98af2be6
CastilloDel <delcastillodelarosadaniel@gmail.com>
2020-03-01 10:00:26 +0000

    Finish implementation
    Had to retouch some parts in the grid

--- src/game_of_life/grid.rs
+++ src/game_of_life/grid.rs
@@ -31,25 +31,31 @@ impl Grid {
     self.gen += 1;
   }
   fn calculate_new_states(&mut self) {
-    for i in 1..self.cells.len() - 2 {
-      for j in 1..self.cells[i].len() - 2 {
+    for i in 1..=self.cells.len() - 2 {
+      for j in 1..=self.cells[i].len() - 2 {
         self.calculate_state(i, j);
       }
     }
   }
   fn calculate_state(&mut self, i :usize, j :usize) {
     let n_neighbours = self.count_neighbours(i, j);
-    if self.cells[i][j].state == States::Alive && !(n_neighbours == 2 || n_neighbours == 3) {
-      self.cells[i][j].state = States::Dead;
-    } else if self.cells[i][j].state == States::Dead && n_neighbours == 3 {
-      self.cells[i][j].state = States::Alive;
+    if self.cells[i][j].state == States::Alive {
+      if n_neighbours == 2 || n_neighbours == 3 {
+        self.cells[i][j].next_state = States::Alive;
+      } else {
+        self.cells[i][j].next_state = States::Dead;
+      }
+    } else if n_neighbours == 3 {
+      self.cells[i][j].next_state = States::Alive;
+    } else {
+      self.cells[i][j].next_state = States::Dead;
     }
   }
   fn count_neighbours(&mut self, i :usize, j :usize) -> u32 {
     let mut count = 0;
-    for k in i - 1..i + 1 {
-      for t in j - 1..j + 1 {
-        if k == t {
+    for k in i - 1..=i + 1 {
+      for t in j - 1..=j + 1 {
+        if k == i && t == j {
           continue;
         }
         if self.cells[k][t].state == States::Alive {
@@ -60,9 +66,9 @@ impl Grid {
     count
   }
   fn update(&mut self) {
-    for i in self.cells.iter_mut() {
-      for j in i.iter_mut() {
-        j.update_state()
+    for i in 1..=self.cells.len() - 2 {
+      for j in 1..=self.cells[i].len() - 2 {
+        self.cells[i][j].update_state()
       }
     }
   }
@@ -74,9 +80,9 @@ impl fmt::Display for Grid {
       for j in i.iter() {
         write!(f, "{}", j)?
       }
-      write!(f, "\n")?
+      writeln!(f)?
     }
-      write!(f, "Generation {}\n", self.gen)
+      writeln!(f, "Generation {}", self.gen)
   }
 }
 
@@ -86,17 +92,35 @@ mod tests {
   #[test]
   fn check_update_grid() {
     let mut grid = Grid::new(5, 5); 
+    grid.change_cell_state(2, 2);
+    grid.change_cell_state(2, 3);
     grid.change_cell_state(3, 3);
-    grid.change_cell_state(3, 4);
-    grid.change_cell_state(4, 4);
     grid.generate_new_gen();
 
-    //let mut grid2 = Grid::new(5, 5); 
-    //grid2.change_cell_state(3, 3);
-    //grid2.change_cell_state(3, 4);
-    //grid2.change_cell_state(4, 4);
-    //grid2.change_cell_state(3, 3);
+    let mut grid2 = Grid::new(5, 5); 
+    grid2.gen = 1;
+    grid2.change_cell_state(2, 2);
+    grid2.change_cell_state(2, 3);
+    grid2.change_cell_state(3, 3);
+    grid2.change_cell_state(3, 2);
+    grid2.calculate_new_states();
 
-    //assert_eq!(grid, grid2);
+    assert_eq!(grid, grid2);
   }
-}
+  #[test]
+  fn check_update_grid2() {
+    let mut grid = Grid::new(3, 3); 
+    grid.change_cell_state(0, 0);
+    grid.change_cell_state(0, 1);
+    grid.change_cell_state(1, 0);
+    grid.generate_new_gen(); 
+
+    let mut grid2 = Grid::new(3, 3); 
+    grid2.gen = 1;
+    grid2.change_cell_state(0, 0);
+    grid2.change_cell_state(0, 1);
+    grid2.change_cell_state(1, 0);
+    grid2.calculate_state(1, 1);
+    grid2.change_cell_state(1, 1);
+   
+    assert_eq!(grid, grid2); } }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,59 @@
 mod game_of_life;
-use game_of_life::grid;
+use game_of_life::grid::Grid;
+use std::{io, time, thread};
+
+const MIN_ROWS: usize = 3;
+const MIN_COLS: usize = 3;
+const MAX_ROWS: usize = 1000;
+const MAX_COLS: usize = 1000;
 
 fn main() {
-   let _game = grid::Grid::new(5, 5); 
+   println!("Number of rows: ");
+   let n_rows = read_number_limits(MIN_ROWS, MAX_ROWS);
+
+   println!("Number of columns: ");
+   let n_columns = read_number_limits(MIN_COLS, MAX_COLS);
+
+   let mut game = Grid::new(n_rows, n_columns); 
+   println!("Enter the number of alive cells:");
+   let n_cells = read_number_limits(0, std::usize::MAX);
+   for _i in 0..n_cells {
+      print!("\x1B[2J");
+      println!("{}", game);
+      println!("Enter the row number:");
+      let row_pos = read_number_limits(1, n_rows);
+      println!("Enter the column number:");
+      let col_pos = read_number_limits(1, n_columns);
+      game.change_cell_state(row_pos - 1, col_pos - 1);
+   }
+   loop {
+      print!("\x1B[2J");
+      println!("{}", game);
+      thread::sleep(time::Duration::from_millis(400));
+      game.generate_new_gen();
+   }
+}
+
+fn read_number_limits(lower_limit: usize, upper_limit: usize) -> usize {
+   loop {
+      let mut number = String::new();
+      io::stdin().read_line(&mut number)
+         .expect("Couldn't read");
+      let number: usize = match number.trim().parse() {
+         Ok(num) if num > upper_limit => {
+            println!("Please enter a number lesser than or equal to {}", upper_limit);
+            continue;
+         },
+         Ok(num) if num < lower_limit => {
+            println!("Please enter a number greater than or equal to {}", lower_limit);
+            continue;
+         },
+         Ok(num) => num,
+         Err(_) => {
+            println!("Please enter a positive number");
+            continue;
+         },
+      };
+      break number
+   }
 }
